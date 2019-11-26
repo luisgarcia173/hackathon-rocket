5f4dbcbe1962ce259cb227725090d7500f67fafc
Thiago Nunes <nunes@tecimob.com.br>
2019-11-26 15:38:40 -0300

    feat(layout) - adicionado layout padrão

--- src/components/LayoutDashboard/styles.js
+++ src/components/LayoutDashboard/styles.js
@@ -1,6 +1,11 @@
 import styled, { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;  
+  }
+  
   html {
     font-size: 14px;
     font-family: Source Sans Pro, sans-serif;  
