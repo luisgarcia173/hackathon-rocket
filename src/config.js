f1b7e4cb2045f4bce874c9ed61d219a22aaf4411
Thiago Nunes <nunes@tecimob.com.br>
2019-11-26 16:42:54 -0300

    feat(GlobalStyle) - adicionado box-sizing

--- src/config.js
+++ src/config.js
@@ -14,7 +14,12 @@ export const padding = {
 export const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
-    padding: 0;  
+    padding: 0; 
+    box-sizing: border-box;
+    
+    &:after,&:before {
+      box-sizing: border-box;
+    }
   }
   
   html {
