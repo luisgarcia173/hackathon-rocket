fded67206c79b352f1b62d0f05f6e6c274dc6462
Lucas Costa Amaral <lucas.costamaral@gmail.com>
2019-11-26 16:50:58 -0300

    feat: Criado component de button

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,9 +1,29 @@
 import styled from 'styled-components';
+import { darken, lighten, readableColor } from 'polished';
 
 // Config
 import { colors, padding } from "config";
 
 export const Wrapper = styled.button`
+  outline: none;
+  cursor: pointer;
   border: 1px solid ${p => colors[p.color]};
   background-color: ${p => colors[p.color]};
+  padding: ${p => padding[p.padding] / 2}px ${p => padding[p.padding]}px;
+  color: ${p => readableColor(colors[p.color], colors.secondary, colors.primary)};
+  
+  transition: transform .18s, background-color .18s, border .18s;
+  will-change: transform, background-color, border;
+  
+  :hover {
+    border: 1px solid ${p => lighten(0.05, colors[p.color])};
+    background-color: ${p => lighten(0.05, colors[p.color])};  
+    transform: scale(1.01);
+  }
+  
+  :active {
+    border: 1px solid ${p => darken(0.05, colors[p.color])};
+    background-color: ${p => darken(0.05, colors[p.color])};  
+    transform: scale(0.99);
+  }
 `;
